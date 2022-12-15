c247ac919468fc927ad491bc3a48de9a62c25026
Atlas1123 <atlas20011123@gmail.com>
2022-12-14 16:51:45 -0800

    feat: add new files in frontend

--- my-app/vue.config.js
+++ my-app/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {
         '^/api': {
-          target: 'http://localhost:3070',
+          target: 'http://localhost:3080',
           changeOrigin: true
         },
       }
